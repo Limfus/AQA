932f5ee196c8d3ee937da2f121a42ef639944a97
Maksym <>
2023-07-17 16:16:44 +0300

    add a homework description and update the  homework 9

--- Lesson09_Task01.py
+++ Lesson09_Task01.py
@@ -1,7 +1,21 @@
 import re
+"""
+Home-task 09 Files operations
+# Write script that will read "input.txt" file
+and find there all characters from english alphabet
+# collect these characters and their positions in file
+# after info collected this info as text
+write to "output.txt" file in such format
+# ####################
+# <first found letter> -> pos1
+# <next_letter> -> pos2
+# <next_letter -> pos3
+# etc
+# #######################
+"""
 
 # Read the input file
-with open("example.txt", "r") as input_file:
+with open('input.txt', 'r') as input_file:
     # Read the contents of the file
     content = input_file.read()
 
@@ -10,13 +24,13 @@ found_letters = re.findall(r"[A-Za-z]", content)
 letter_positions = [(char, content.index(char) + 1) for char in found_letters]
 
 # Write the collected information to the output file
-with open("output.txt", "w") as output_file:
+with open('output.txt', 'w') as output_file:
     # Write the header
-    output_file.write("#########################\n")
+    output_file.write('#########################\n')
 
     # Write the characters and their positions
     for letter, position in letter_positions:
-        output_file.write(f"{letter} -> pos{position}\n")
+        output_file.write(f'{letter} -> pos{position}\n')
 
     # Write the footer
-    output_file.write("#########################")
+    output_file.write('#########################')

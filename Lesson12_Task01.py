2c356b57132f4168e01dd757b9bddee1ccf5c254
Maksym <>
2023-08-07 17:55:40 +0300

    Updated the Homework 12 Recursion

--- Lesson12_Task01.py
+++ Lesson12_Task01.py
@@ -9,23 +9,17 @@ Output: Target sum = 72
 """
 
 
-def iterative_integer_generator(target_list):
-    stack = [target_list]
-    while stack:
-        current_item = stack.pop()
-        if isinstance(current_item, int):
-            yield current_item
-        elif isinstance(current_item, list):
-            stack.extend(reversed(current_item))
-
-
-def get_target_sum(target_list):
-    integer_iterator = iterative_integer_generator(target_list)
-    target_sum = sum(integer_iterator)
-    return target_sum
+def recursive_list_sum(target_list):
+    total_sum = 0
+    for item in target_list:
+        if isinstance(item, int):
+            total_sum += item
+        elif isinstance(item, list):
+            total_sum += recursive_list_sum(item)
+    return total_sum
 
 
 # Test the function with the given input
 input_list = [[[[1, 4, 5], [[6, 9], [[[8, 1], 7], 3], 2], 7], 5, 2], 9, [1, 2]]
-output_sum = get_target_sum(input_list)
+output_sum = recursive_list_sum(input_list)
 print("Target sum =", output_sum)  # Output: Target sum = 72
